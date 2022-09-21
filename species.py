2a6b39d6f5aae40cd9a534a7bc206da307fa3af4
Sam Pohlenz <sam@sampohlenz.com>
2022-09-21 14:47:28 +0930

    Add CSV output format type option for species.py

--- species.py
+++ species.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import operator
 import argparse
 
 import config as cfg
@@ -17,16 +18,37 @@ def getSpeciesList(lat, lon, week, threshold=0.05, sort=False):
     slist = []
     for p in pred:
         if p[0] >= threshold:
-            slist.append(p[1])
+            slist.append(p)
 
     print('Done. {} species on list.'.format(len(slist)), flush=True)
 
     if sort:
-        slist = sorted(slist)
+        slist = sorted(slist, key=operator.itemgetter(1))
 
     return slist
 
-if __name__ == '__main__':    
+def saveSpeciesList(results, path):
+    out_string = ''
+
+    if cfg.RESULT_TYPE == 'csv':
+        # CSV output file
+        header = 'Scientific name,Common name,Probability\n'
+
+        # Write header
+        out_string += header
+
+        for s in results:
+            out_string += '{},{},{}\n'.format(s[1].split('_')[0], s[1].split('_')[1], s[0])
+
+    else:
+        for s in results:
+            out_string += s[1] + '\n'
+
+    with open(path, 'w') as rfile:
+        rfile.write(out_string)
+
+
+if __name__ == '__main__':
 
     # Parse arguments
     parser = argparse.ArgumentParser(description='Get list of species for a given location with BirdNET. Sorted by occurrence frequency.')
@@ -36,7 +58,8 @@ if __name__ == '__main__':
     parser.add_argument('--week', type=int, default=-1, help='Week of the year when the recording was made. Values in [1, 48] (4 weeks per month). Set -1 for year-round species list.')
     parser.add_argument('--threshold', type=float, default=0.05, help='Occurrence frequency threshold. Defaults to 0.05.')
     parser.add_argument('--sortby', default='freq', help='Sort species by occurrence frequency or alphabetically. Values in [\'freq\', \'alpha\']. Defaults to \'freq\'.')
-    
+    parser.add_argument('--rtype', default='list', help='Specifies output format. Values in [\'list\', \'csv\']. Defaults to \'list\' (Species list).')
+
     args = parser.parse_args()
 
     # Set paths relative to script path (requested in #3)
@@ -51,6 +74,11 @@ if __name__ == '__main__':
     if os.path.isdir(cfg.OUTPUT_PATH):
         cfg.OUTPUT_PATH = os.path.join(cfg.OUTPUT_PATH, 'species_list.txt')
 
+    # Set result type
+    cfg.RESULT_TYPE = args.rtype.lower()
+    if not cfg.RESULT_TYPE in ['list', 'csv']:
+        cfg.RESULT_TYPE = 'list'
+
     # Set config
     cfg.LATITUDE, cfg.LONGITUDE, cfg.WEEK = args.lat, args.lon, args.week
     cfg.LOCATION_FILTER_THRESHOLD = args.threshold
@@ -59,9 +87,7 @@ if __name__ == '__main__':
     species_list = getSpeciesList(cfg.LATITUDE, cfg.LONGITUDE, cfg.WEEK, cfg.LOCATION_FILTER_THRESHOLD, False if args.sortby == 'freq' else True)
 
     # Save species list
-    with open(cfg.OUTPUT_PATH, 'w') as f:
-        for s in species_list:
-            f.write(s + '\n')
+    saveSpeciesList(species_list, cfg.OUTPUT_PATH)
 
     # A few examples to test
     # python3 species.py --o example/ --lat 42.5 --lon -76.45 --week -1
